d578176b2f1ea589e2fbfc3387018badfa321aeb
matt1610 <mattstarkey@me.com>
2015-09-10 15:31:53 +0200

    API Path

--- public/js/services.js
+++ public/js/services.js
@@ -45,8 +45,8 @@ FormBuilder.factory('API', function($http, APIPATH, Local, Base64){
 })
 
 .factory('APIPATH', function(){
-	return 'http://localhost:5000';
-    // return 'http://applififormbuilder.azurewebsites.net';
+	// return 'http://localhost:5000';
+    return 'http://applififormbuilder.azurewebsites.net';
 })
 
 .factory('Local', function(){
